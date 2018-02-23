b839a9985ca22efc6c8c5eff5f79d5c16b1fe35a
Kent <le6790@gmail.com>
2018-02-23 00:35:23 -0700

    initial commit. It works but not correctly

---
+++ app/src/main/java/com/example/android/workout/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.android.workout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView workoutCategories;
+    public final void openWorkout1(View view){
+        Intent intent = new Intent (this, DisplayWorkout2.class);
+        startActivity(intent);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        workoutCategories = (TextView) findViewById(R.id.workout_categories);
+
+        String[] workoutCats = WorkoutCategories.getWorkoutCategories();
+
+        for (String cat : workoutCats){
+            workoutCategories.append(cat + "\n \n \n");
+        }
+
+        workoutCategories.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                openWorkout1(v);
+            }
+        });
+
+
+    }
+
+}
